3c85ee6c1d70814060c6c63e9b6c60fcee96d66d
jmacalupur <macalupu.j@gmail.com>
2018-04-03 20:33:15 -0500

    imagenes subidas

--- 02_potenciometroLed/02_potenciometroLed.ino
+++ 02_potenciometroLed/02_potenciometroLed.ino
@@ -3,13 +3,13 @@ int potPin = A0;
 int potValue = 0;
 
 void setup() {
-pinMode(ledRojo, OUTPUT);
+pinMode(ledRojo, OUTPUT); //indicamos que el puerto "ledRojo" es de salida
 }
 
 void loop() {
-potValue = analogRead(potPin); 
-digitalWrite(ledRojo, HIGH);
-delay(potValue);
-digitalWrite(ledRojo, LOW);
-delay(potValue);
+potValue = analogRead(potPin); //asignamos a la variable potValue el valor que leerá el puerto A0 (de 0 a 1023)
+digitalWrite(ledRojo, HIGH); //escribimos el valor HIGH (o encendido) en el led.
+delay(potValue); //damos una demora que es igual al valor de potValue (de 0 a 1023)
+digitalWrite(ledRojo, LOW); //escribimos el valor LOW (o apagado) en el led.
+delay(potValue); //damos una demora que es igual al valor de potValue (de 0 a 1023)
 }
